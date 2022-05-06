6cc363129a7ef79849b3470533122ccbb3d140d0
RichSilva <richsilva07@outlook.com>
2022-05-06 18:02:02 -0300

    Função para permitir só números no input

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,14 @@
+function SomenteNumero(e){
+    var tecla=(window.event)?e.keyCode:e.which;   
+    if((tecla>47 && tecla<58)) return true;
+    else{
+    	if (tecla==8 || tecla==0) return true;
+	else  return false;
+    }
+}
 
-let calcForm = document.getElementById('formulario'),
-    display = document.getElementById('display');
+
+let display = document.getElementById('display');
 
 let firstValue, secondValue, operator, conta, resultado;
 
@@ -40,6 +48,8 @@ numeros.forEach(numero => numero.addEventListener('click', function(e) {
         case 'numberZero':
             display.value += 0;
             break;
+        default:
+            window.alert('Error');
     }
 }))
 
